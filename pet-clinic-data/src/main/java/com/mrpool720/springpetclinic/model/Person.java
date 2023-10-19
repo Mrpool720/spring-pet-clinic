18f124fe394c6cb657ee5ce356082b2c76aa79d5
Mrpool720 <mrpool720@gmail.com>
2023-10-19 22:20:00 +0530

    Add BaseEntity for IDs.

--- pet-clinic-data/src/main/java/com/mrpool720/springpetclinic/model/Person.java
+++ pet-clinic-data/src/main/java/com/mrpool720/springpetclinic/model/Person.java
@@ -1,6 +1,6 @@
 package com.mrpool720.springpetclinic.model;
 
-public class Person {
+public class Person extends BaseEntity{
 
     private String firstName;
     private String lastName;
--- pet-clinic-data/src/main/java/com/mrpool720/springpetclinic/model/PetType.java
+++ pet-clinic-data/src/main/java/com/mrpool720/springpetclinic/model/PetType.java
@@ -1,6 +1,6 @@
 package com.mrpool720.springpetclinic.model;
 
-public class PetType {
+public class PetType extends BaseEntity{
 
     private String petName;
 
